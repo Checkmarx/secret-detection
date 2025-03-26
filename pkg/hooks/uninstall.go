cc602df547b0e22903e8d6c08fac858e9c6dd42d
arturfalcao <celso.silva@checkmarx.com>
2025-03-26 12:47:01 +0000

    feat: bump uninstall

--- pkg/hooks/uninstall.go
+++ pkg/hooks/uninstall.go
@@ -2,6 +2,8 @@ package hooks
 
 import (
 	"fmt"
+	"github.com/Checkmarx/secret-detection/pkg/config"
+	"gopkg.in/yaml.v2"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -18,19 +20,45 @@ func Uninstall(global bool) error {
 
 // uninstallLocal removes pre-commit hooks from the current Git repository.
 func uninstallLocal() error {
-	fmt.Println("Uninstalling local pre-commit hooks...")
+	fmt.Println("Uninstalling cx-secret-detection hook...")
 
-	if !isGitRepo() {
-		return fmt.Errorf("current directory is not a Git repository")
-	}
-
-	cmd := exec.Command("pre-commit", "uninstall")
-	output, err := cmd.CombinedOutput()
+	// Read the .pre-commit-config.yaml file
+	data, err := os.ReadFile(".pre-commit-config.yaml")
 	if err != nil {
-		return fmt.Errorf("failed to uninstall local pre-commit hooks: %v\n%s", err, output)
+		return fmt.Errorf("failed to read .pre-commit-config.yaml: %v", err)
 	}
 
-	fmt.Println(string(output))
+	// Unmarshal the YAML data into a PreCommitConfig object
+	var preCommitConfig config.PreCommitConfig
+	err = yaml.Unmarshal(data, &preCommitConfig)
+	if err != nil {
+		return fmt.Errorf("failed to unmarshal YAML: %v", err)
+	}
+
+	// Remove the cx-secret-detection hook from the repos
+	for i, repo := range preCommitConfig.Repos {
+		var updatedHooks []config.Hook
+		for _, hook := range repo.Hooks {
+			if hook.ID != "cx-secret-detection" {
+				updatedHooks = append(updatedHooks, hook)
+			}
+		}
+		preCommitConfig.Repos[i].Hooks = updatedHooks
+	}
+
+	// Marshal the updated PreCommitConfig object back to YAML
+	updatedData, err := yaml.Marshal(preCommitConfig)
+	if err != nil {
+		return fmt.Errorf("failed to marshal YAML: %v", err)
+	}
+
+	// Write the updated YAML data back to the .pre-commit-config.yaml file
+	err = os.WriteFile(".pre-commit-config.yaml", updatedData, 0644)
+	if err != nil {
+		return fmt.Errorf("failed to write .pre-commit-config.yaml: %v", err)
+	}
+
+	fmt.Println("cx-secret-detection hook uninstalled successfully.")
 	return nil
 }
 
